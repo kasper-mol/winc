2739163b3bec4f9d9029dc12bd42c921c8e75dd5
Kasper Mol <kaspermol1994@gmail.com>
2020-06-26 14:02:29 +0200

    Eindopdracht af

--- Week_2/W2D5/script.js
+++ Week_2/W2D5/script.js
@@ -5,6 +5,7 @@ const avengersButton = document.getElementById("avengers")
 const xmenButton = document.getElementById("x-men");
 const princessButton = document.getElementById("princess");
 const batmanButton = document.getElementById("batman");
+const zoekbalk = document.getElementById("zoekbalk");
 
 // Make the movielist
 const makeMovieList = moviesDatabase.forEach(Element => {
@@ -14,22 +15,11 @@ const makeMovieList = moviesDatabase.forEach(Element => {
 })
 
 // Filters
-const newOnly = (film) => {
-    return parseInt(film.Year) >= 2014
-}
-const avengersOnly = (film) => {
-    return film.Title.includes("Avengers");
-}
-const xmenOnly = (film) => {
-    return film.Title.includes("X-Men");
-}
-const princessOnly = (film) => {
-    return film.Title.includes("Princess");
-}
-const batmanOnly = (film) => {
-    return film.Title.includes("Batman");
-}
-
+const newOnly = (film) => { return parseInt(film.Year) >= 2014 }
+const avengersOnly = (film) => { return film.Title.includes("Avengers"); }
+const xmenOnly = (film) => { return film.Title.includes("X-Men"); }
+const princessOnly = (film) => { return film.Title.includes("Princess"); }
+const batmanOnly = (film) => { return film.Title.includes("Batman"); }
 
 // switch for diffferent filters
 const filterMovies = (x) => {
@@ -94,9 +84,25 @@ const filterMovies = (x) => {
             }
             )
             break;
+        case 5:  // only batman movies
+            movieList.querySelectorAll("img").forEach(n => n.remove());
+            const zoekFilter = (film) => {
+                let zoekLijst = film.Title.toLowerCase();
+                let zoekCritera = zoekbalk.value.toLowerCase();
+                return zoekLijst.includes(zoekCritera);
+            }
+            const zoeken = moviesDatabase.filter(zoekFilter);
+            zoeken.forEach(Element => {
+                let newPoster = document.createElement("img");
+                let link = document.createElement("a");
+                movieList.appendChild(document.createElement("li").appendChild(link)).appendChild(newPoster);
+                newPoster.setAttribute("src", Element.Poster);
+                link.setAttribute("href", "https://www.imdb.com/title/" + Element.imdbID);
+            }
+            )
+            break;
     }
 }
-
 
 // Eventlistners
 nieuweButton.addEventListener("click", filterMovies("new"));
